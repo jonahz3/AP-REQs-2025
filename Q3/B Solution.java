1e8403004394af95978db7baed6d9b2ec1c28fc9
jonahz3 <Jonahz3@nycstudents.net>
2025-05-15 11:42:09 -0400

    Update B Solution.java

--- Q3/B Solution.java
+++ Q3/B Solution.java
@@ -1,1 +1,12 @@
-
+public ArrayList<Match> buildMatches(){
+    ArrayList<Match> result = new ArrayList();
+    ArrayList<Competitor> newList = competitorList;
+    if(newList.size()%2!=0){
+        newList.remove(0);
+    }
+    for(int x = 0; x>newList.size()/2;x++){
+        Match obj1 = new Match(newList.get(x), newList.get((newList.size()-x));
+        result.add(obj1);
+    }
+    return result;
+}
