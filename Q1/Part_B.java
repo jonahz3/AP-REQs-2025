a2ee4b2b19f24bd1fc37eab359c6b754b57978bf
jonahz3 <Jonahz3@nycstudents.net>
2025-05-21 11:36:07 -0400

    Update Part_B.java

--- Q1/Part_B.java
+++ Q1/Part_B.java
@@ -1,1 +1,12 @@
-
+public int dogWalkShift(int startHour, int endHour){
+  int result = 0;
+  
+  for(int x = startHour; x<=endHour; x++){
+    int numWalk = walkDogs(x);
+    result += 5*3
+    if(numWalk=this.maxDogs){
+      result+=3;
+    }
+  }
+  return result;
+}
