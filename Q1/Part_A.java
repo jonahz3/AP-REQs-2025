740e56d1e97cc6b01afa2bb7aa3e8252cc0e1962
jonahz3 <Jonahz3@nycstudents.net>
2025-05-21 11:23:49 -0400

    Update Part_A.java

--- Q1/Part_A.java
+++ Q1/Part_A.java
@@ -1,1 +1,11 @@
+public int walkDogs(int hour){
+  int result;
+  if(DogWalkCompany.numAvailableDogs()>=this.maxDogs){
+    result = this.maxDogs;
+  } else {
+    result = DogWalkCompany.numAvailableDogs();
+  }
 
+  updateDogs(hour, result);
+  return result;
+}
