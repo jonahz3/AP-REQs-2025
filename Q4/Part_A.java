73fae4076030466ee99612b2f448fb6b6750e12f
jonahz3 <Jonahz3@nycstudents.net>
2025-05-21 11:44:26 -0400

    Update Part_A.java

--- Q4/Part_A.java
+++ Q4/Part_A.java
@@ -1,1 +1,8 @@
-
+public SumOrSameGame(int numRows, int numCols){
+  puzzle = new int[numRows][numCols];
+  for(int a = 0; a<numRow; a++) {
+      for(int b = 0; b<numCols; b++) {
+        puzzle[a][b] = (int)(Math.random()*9+1);
+      }
+  }
+}
